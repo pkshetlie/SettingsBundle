ca48583f59b0b9d5d95d101665ab4d4f0cfe07ac
pierrick pobelle <pierrick.pobelle@gmail.com>
2018-11-15 14:19:23 +0100

    ajout d'une extension de controller

---
+++ Controller/ControllerWithSettings.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p.pobelle
+ * Date: 15/11/2018
+ * Time: 14:10
+ */
+
+namespace Pkshetlie\SettingsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+abstract class ControllerWithSettings extends Controller
+{
+    private $setting_service = null;
+
+    /**
+     * @param string $name name of the setting
+     * @param mixed $default default value to return
+     * @return mixed|null|string
+     */
+    public function getSetting($name, $default)
+    {
+        if(null === $this->setting_service){
+            $this->setting_service = $this->get('pkshetlie.settings');
+        }
+        return $this->setting_service->get($name, $default);
+    }
+
+}
